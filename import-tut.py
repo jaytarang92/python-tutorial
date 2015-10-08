7bb4c722ad0c6d48e575ef88efd97752249cbd9e
Jasmit Tarang <J_Hack92@Jasmits-Mac-Pro.local>
2015-10-08 16:30:50 -0500

    Adding a import tutorial

--- import-tut.py
+++ import-tut.py
@@ -1,1 +1,10 @@
 __author__ = 'J_Hack92'
+
+from classes import *
+
+fh = FileHandler("tutorial.txt", "this is self.crap yo!!")
+fh.write2file()
+
+"""
+    So now just with 3 lines of code we have a text file with data on it.
+"""
